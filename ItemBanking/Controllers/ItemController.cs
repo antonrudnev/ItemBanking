6dd95d736084f7358138da5ae71b568d807a93f2
Anton Rudnev <anton.rudnev@gmail.com>
2019-12-05 12:48:13 -0800

    Edit usecase implemented for different scripts.

--- ItemBanking/Controllers/ItemController.cs
+++ ItemBanking/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using ItemBanking.Data;
+using ItemBanking.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -18,10 +19,51 @@ namespace ItemBanking.Controllers
             _logger = logger;
         }
 
-        public async Task<IActionResult> Index(int id)
+        public async Task<IActionResult> Index(int? id)
         {
-            var itemBank = await _context.ItemBanks.Where(x => x.Id == id).Include("Language").Include("Categories.Items").SingleAsync();
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var itemBank = await _context.ItemBanks.Include("Categories.Items").Include("Language").SingleOrDefaultAsync(x => x.Id == id);
+            if (itemBank == null)
+            {
+                return NotFound();
+            }
             return View(itemBank);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Items.Include("Category.ItemBank.Language").SingleOrDefaultAsync(x => x.Id == id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Content")]Item item)
+        {
+            if (id != item.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                int categoryId = await _context.Items.Where(x => x.Id == id).Select(x => x.Category.ItemBank.Id).SingleOrDefaultAsync();
+                return RedirectToAction(nameof(Index), new { id = categoryId });
+            }
+            return View(item);
         }
     }
 }
